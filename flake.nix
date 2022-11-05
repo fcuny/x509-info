6a72b1102f59c0c87b64fd818a2963e3e40be7f7
Franck Cuny <franck@fcuny.net>
2022-11-04 18:11:43 -0700

    buid: use crane to build the binary
    crane is a nix library to build cargo projects, similar to naersk. The
    main advantage over naersk is the composability.
    
    I'll give it a try and see if it's faster for local development.

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs = {
@@ -25,43 +29,45 @@
     , flake-utils
     , nixpkgs
     , rust-overlay
+    , crane
     , pre-commit-hooks
     }:
-    let
-      # Borrow project metadata from the Rust config
-      meta = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
-      inherit (meta) name version;
 
-      overlays = [
-        # Rust helpers
-        (import rust-overlay)
-        # Build Rust toolchain using helpers from rust-overlay
-        (self: super: {
-          # This supplies cargo, rustc, rustfmt, etc.
-          rustToolchain = super.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        })
-      ];
-    in
     flake-utils.lib.eachDefaultSystem
       (system:
       let
-        pkgs = import nixpkgs { inherit overlays system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        craneLib = (crane.mkLib pkgs).overrideScope' (_: _: {
+          cargo = rust-toolchain;
+          clippy = rust-toolchain;
+          rustc = rust-toolchain;
+          rustfmt = rust-toolchain;
+        });
+
+        src = ./.;
+
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src;
+        };
+
+        x509-info = craneLib.buildPackage {
+          inherit cargoArtifacts src;
+        };
       in
       {
-        packages = rec {
-          default = x509-info;
-          x509-info = pkgs.rustPlatform.buildRustPackage {
-            pname = name;
-            inherit version;
-            src = ./.;
-            release = true;
-            cargoLock.lockFile = ./Cargo.lock;
-          };
+        packages.default = x509-info;
+        apps.default = flake-utils.lib.mkApp {
+          drv = x509-info;
         };
 
         checks = {
           pre-commit = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
+            inherit src;
             hooks = {
               clippy = {
                 enable = true;
@@ -80,10 +86,8 @@
 
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            rustToolchain
-            cargo-audit
+            rust-toolchain
             cargo-deny
-            rust-analyzer
           ];
 
           inherit (self.checks.${system}.pre-commit) shellHook;
